5c1251e8d1b317b715b25c5c4b7d5e5789315cda
Leonid <ithedgehog@yandex.ru>
2022-08-22 00:54:55 +0300

    It's work! :-)

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,17 +1,39 @@
+import java.util.Random;
+
 public class MagicBox<T> {
+    protected T[] items;
+    int total;
 
-
-    protected int size;
-    protected int items[];
-
-
+    public MagicBox(int size) {
+        this.items = (T[]) new Object[size];
+    }
 
     public boolean add(T item) {
-        if (item != null) {
-            item += items;
-            true;
-        } else (item == null) {
-            false;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                items[i] = item;
+                total++;
+                return true;
+            }
         }
+        return false;
+    }
+
+    public T pick() {
+        Random random = new Random();
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null) {
+                throw new RuntimeException("Box is not full, add " + (items.length - total) + " items!");
+            }
+        }
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                int randomInt = random.nextInt(items.length);
+                System.out.println("Box is full! Random element: " + items[randomInt]);
+                System.out.println();
+                return items[randomInt];
+            }
+        }
+        return null;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int size = 5;
-        T[] items = (T[]) new Object[size];
+        MagicBox<String> testString = new MagicBox<>(5);
+        String boxString[] = {"Python", "Java", "JS", "Haskell", "C++"};
+        testString.add(boxString[0]);
+        testString.add(boxString[1]);
+        testString.add(boxString[2]);
+        testString.add(boxString[3]);
+        testString.add(boxString[4]);
+        testString.pick();
+
+        MagicBox<Integer> testInteger = new MagicBox<>(5);
+        int boxInteger[] = {404, 502, 666, 90, 777};
+        testInteger.add(boxInteger[0]);
+        testInteger.add(boxInteger[1]);
+        testInteger.add(boxInteger[2]);
+        testInteger.add(boxInteger[3]);
+        testInteger.add(boxInteger[4]);
+        testInteger.pick();
     }
 }
